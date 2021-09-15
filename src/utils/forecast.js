2865fab1736f58daf9ab0b4ec4d9387504157bf3
Nir Turkenitz <nir.turkenitz@asperii.com>
2021-09-15 23:58:43 +0300

    Changes for forecast string

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,11 @@ const forecast = (longitude, latitude, callback) => {
         } else if (body.error) {
             callback('Unable to find weather for the given location');
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + '. It feels like ' + body.current.feelslike + ' degrees out.');
+            callback(undefined,
+                body.current.weather_descriptions[0] +
+                '. It is currently ' + body.current.temperature +
+                '. It feels like ' + body.current.feelslike + ' degrees out' +
+                '. The humidity is ' + body.current.humidity + '%');
         }
     })
 }
